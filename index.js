fbe519aa10949273fed1d06247b233650f381031
reelmza <jessemoses71@gmail.com>
2020-12-13 10:50:58 +0100

    Added mobile navbar

--- index.js
+++ index.js
@@ -22,3 +22,22 @@ var swiper = new Swiper('.swiper-container', {
     }
 })
 var swiper = document.querySelector('.swiper-container').swiper
+
+// Mobile nav-bar functions
+const navbutton = document.getElementById('mobile-nav-icon')
+const mobileNav = document.getElementsByClassName('mobile-navbar')[0]
+let mobileNavState = 0
+
+navbutton.addEventListener('click', () => {
+    console.log("clicked")
+
+    if (mobileNavState == 0) {
+        mobileNav.classList.remove('hidden')
+        return mobileNavState = 1
+    }
+
+    if (mobileNavState == 1) {
+        mobileNav.classList.add('hidden')
+        return mobileNavState = 0
+    }
+}) 
