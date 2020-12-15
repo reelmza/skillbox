d8cd396a922a602aaac79fb6f809fb333279de28
reelmza <jessemoses71@gmail.com>
2020-12-15 02:36:24 +0100

    Removed progress from mobile view

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// Mobile nav-bar functions
+const navbutton = document.getElementsByClassName('mobile-nav-icon')[0]
+const mobileNav = document.getElementsByClassName('mobile-navbar')[0]
+const navIconChild = document.getElementsByClassName('nav-icon-child')[0]
+let mobileNavState = 0
+
+navbutton.addEventListener('click', () => {
+    console.log("clicked")
+
+    if (mobileNavState == 0) {
+        mobileNav.classList.remove('hidden')
+        navIconChild.classList.remove('fa-bars')
+        navIconChild.classList.add('fa-times')
+        return mobileNavState = 1
+    }
+
+    if (mobileNavState == 1) {
+        navIconChild.classList.remove('fa-times')
+        navIconChild.classList.add('fa-bars')
+
+
+        mobileNav.classList.add("hidden")
+        return mobileNavState = 0
+
+    }
+})
+
 // Carousel function with @swiperjs.com
 var swiper = new Swiper('.swiper-container', {
     init: true,
@@ -22,35 +49,3 @@ var swiper = new Swiper('.swiper-container', {
     }
 })
 var swiper = document.querySelector('.swiper-container').swiper
-
-// Mobile nav-bar functions
-const navbutton = document.getElementsByClassName('mobile-nav-icon')[0]
-const mobileNav = document.getElementsByClassName('mobile-navbar')[0]
-const navIconChild = document.getElementsByClassName('nav-icon-child')[0]
-let mobileNavState = 0
-
-navbutton.addEventListener('click', () => {
-    console.log("clicked")
-
-    if (mobileNavState == 0) {
-        mobileNav.classList.remove('hidden')
-        navIconChild.classList.remove('fa-bars')
-        navIconChild.classList.add('fa-times')
-
-        mobileNav.classList.add('slideIn')
-        return mobileNavState = 1
-    }
-
-    if (mobileNavState == 1) {
-        navIconChild.classList.remove('fa-times')
-        navIconChild.classList.add('fa-bars')
-
-        mobileNav.classList.add("slideOut")
-
-        setTimeout(() => {
-            mobileNav.classList.add("hidden")
-            mobileNav.classList.remove("slideOut")
-            return mobileNavState = 0
-        }, 300)
-    }
-}) 
