a173d7c9a50321d4d73d9714b4e6ce9e545223c7
reelmza <jessemoses71@gmail.com>
2020-12-16 18:52:34 +0100

    Added button border

--- index.js
+++ index.js
@@ -2,22 +2,34 @@
 const navbutton = document.getElementsByClassName('mobile-nav-icon')[0]
 const mobileNav = document.getElementsByClassName('mobile-navbar')[0]
 const navIconChild = document.getElementsByClassName('nav-icon-child')[0]
+const don = document.getElementsByClassName('don')
+
+
+// Declare mobile-navigation at start
 let mobileNavState = 0
 
 navbutton.addEventListener('click', () => {
     console.log("clicked")
 
     if (mobileNavState == 0) {
+        for (let i = 0; i < don.length; i++) {
+            don[i].classList.add('disabled')
+        }
+
         mobileNav.classList.remove('hidden')
+
         navIconChild.classList.remove('fa-bars')
         navIconChild.classList.add('fa-times')
         return mobileNavState = 1
     }
 
     if (mobileNavState == 1) {
+        for (let i = 0; i < don.length; i++) {
+            don[i].classList.remove('disabled')
+        }
+
         navIconChild.classList.remove('fa-times')
         navIconChild.classList.add('fa-bars')
-
 
         mobileNav.classList.add("hidden")
         return mobileNavState = 0
